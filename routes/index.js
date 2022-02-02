a3302ab80741b2fbfc6e226e45ad074c9da8f3fc
Gurleen1601 <gurleen0790.cse19@chitkara.edu.in>
2022-02-03 02:58:56 +0530

    Added users/profilecontroller

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,8 @@ const router=express.Router();
 const homeController=require('../controllers/home_controller');
 router.get('/',homeController.home);
 
+router.use('/users',require('./users'));
+
+// for any further routes,access from here
+// router.use('/routerName',require('./routerfile'));
 module.exports=router;
