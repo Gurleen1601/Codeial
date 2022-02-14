c2d7729c5bf3c20cae12032fef9b2852476714b1
Gurleen1601 <gurleen0790.cse19@chitkara.edu.in>
2022-02-15 03:21:19 +0530

    passport

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const app=express();
 const port=8000;
 const expressLayouts=require('express-ejs-layouts');
 const db=require('./config/mongoose');
+// used for session cookie
 const session=require('express-session');
 const passport=require('passport');
-const LocalStrategy=require('passport-local');
+const passportLocal=require('./config/passport-local-strategy');
 
 app.use(express.urlencoded());
 
- app.use(cookieParser());  
+app.use(cookieParser());  
 
 // using static files
 app.use(express.static('./assets'));
@@ -27,6 +28,20 @@ app.use('/',require('./routes'));
 // set up the views engine
 app.set('view engine','ejs');
 app.set('views','./views');
+
+app.use(session({
+    name:'Codeial',
+    // todo change the secret before deployment in production mode
+    secret:'blahsomething',
+    saveUninitialized:false,
+    resave:false,
+    cookie:{
+        maxAge:(1000 * 60 * 100)
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 app.listen(port,function(err){
