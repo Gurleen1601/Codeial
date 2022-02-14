ba4036e99dcca6057b6c18028648d2cc58234cea
Gurleen1601 <gurleen0790.cse19@chitkara.edu.in>
2022-02-15 03:44:09 +0530

    changes

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -66,30 +66,7 @@ module.exports.create=function(req,res){
 // sign in and create a session for the user 
 module.exports.createSession=function(req,res){
     
-    // steps to authenticate 
-    // find the user
-    User.findOne({
-        email:req.body.email
-    },function(err,user){
-        if(err){
-            console.log('error in finding user in sign-in');
-            return;
-        }
-    //handle user found
-     if(user){
-   // handle passwrord which don't match
-     if(user.password!=req.body.password){
-         return res.redirect('back');
-     }
-   // handle session creation
-   res.cookie('user_id',user.id);
-   return res.redirect('/users/profile');
-     }else{
-     //handle user not found
-     return res.redirect('back');
-     }
-
-    });
+   
 }
 // remove cookies when the user has signed out
 module.exports.signOut=function(req,res){
