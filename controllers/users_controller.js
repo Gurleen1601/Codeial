82db86312757d8bf02c0079758cd88c2db13c3b0
Gurleen1601 <gurleen0790.cse19@chitkara.edu.in>
2022-02-15 19:57:38 +0530

    Added sign out

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -58,5 +58,8 @@ module.exports.create=function(req,res){
 // sign in and create a session for the user 
 module.exports.createSession=function(req,res){
     return res.redirect('/');
-   
+}
+module.exports.destroySession=function(req,res){
+    req.logout();
+    return res.redirect('/');
 }
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const db=require('./config/mongoose');
 const session=require('express-session');
 const passport=require('passport');
 const passportLocal=require('./config/passport-local-strategy');
+const MongoStore=require('connect-mongo')(session);
 
 app.use(express.urlencoded());
 
@@ -26,6 +27,7 @@ app.set('layout extractStyles',true);
 app.set('view engine','ejs');
 app.set('views','./views');
 
+// mongo store is used to store the session cookie in the db
 app.use(session({
     name:'Codeial',
     // todo change the secret before deployment in production mode
@@ -34,7 +36,17 @@ app.use(session({
     resave:false,
     cookie:{
         maxAge:(1000 * 60 * 100)
-    }
+    },
+    store: new MongoStore(
+        {
+            mongooseConnection:db,
+            autoRemove:'disabled'
+        },function(err){
+            if(err){
+                console.log(err || 'connect-mongodb setup ok');
+            }
+        }
+    )
 }));
 
 app.use(passport.initialize());
