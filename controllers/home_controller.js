2ac09866795b5ad268926a175efb618c8816cbea
Gurleen1601 <gurleen0790.cse19@chitkara.edu.in>
2022-02-23 02:17:25 +0530

    displayed author of the  post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -4,13 +4,19 @@ module.exports.home=function(req,res){
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
  
-    Post.find({},function(err,posts){
+    // Post.find({},function(err,posts){
+    //     return res.render('home',{
+    //         title:"Codeial | Home" ,
+    //         posts:posts
+    //     });
+    // });
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function(err,posts){
         return res.render('home',{
-            title:"Codial | Home" ,
+            title:"Codeial | Home" ,
             posts:posts
         });
     });
-
 }
 
 //module.exports.actionName=function(req,res){}
