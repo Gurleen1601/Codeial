e33f01f4750d370d6c0749b4ad8492da270f16d2
Gurleen1601 <gurleen0790.cse19@chitkara.edu.in>
2022-02-23 03:31:42 +0530

    added comments to the db

--- models/comment.js
+++ models/comment.js
@@ -10,7 +10,7 @@ const commentSchema=new mongoose.Schema({
         type:mongoose.Schema.Types.ObjectId,
         ref:'User'
     },
-    posts:{
+    post:{
         type:mongoose.Schema.Types.ObjectId,
         ref:'Post'
     }
