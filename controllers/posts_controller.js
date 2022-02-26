9b9d2d5f45cb630f27cf26fc9fe35f70c52f180d
Gurleen1601 <gurleen0790.cse19@chitkara.edu.in>
2022-02-26 14:50:34 +0530

    deleted a post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
-const Post=require('../models/post')
+const Post=require('../models/post');
+const Comment = require('../models/comment');
 module.exports.create=function(req,res){
     Post.create({
        content:req.body.content,
@@ -10,4 +11,20 @@ module.exports.create=function(req,res){
         return res.redirect('back');
     });
 
+}
+
+module.exports.destroy=function(req,res){
+    Post.findById(req.params.id,function(err,post){
+     //automatic coversion of id into string -> .id
+     if(post.user == req.user.id ){
+           post.remove();
+           Comment.deleteMany({
+               post:req.params.id
+           },function(err){
+             return res.redirect('back');
+           });
+     }else{
+         res.redirect('back');
+     }
+    });
 }
