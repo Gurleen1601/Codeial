569a46cb080098c18ebc7d5d4461a65325335b0d
Gurleen1601 <gurleen0790.cse19@chitkara.edu.in>
2022-03-08 16:15:11 +0530

    generated api

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -1,1 +1,7 @@
-// this file is for handling all the routes for a particular version like 'api/v1/post' 
+// this file is for handling all the routes for a particular version like 'api/v1/post'
+const express=require('express');
+const router=express.Router();
+
+router.use('/posts',require('./posts'));
+
+module.exports=router;
