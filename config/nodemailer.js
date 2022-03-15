ca4a616455c599c4e836aea32fc80b20763cb794
Gurleen1601 <gurleen0790.cse19@chitkara.edu.in>
2022-03-15 21:21:47 +0530

    sent mails

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -9,7 +9,7 @@ let transporter=nodeMailer.createTransport({
     port:587,
     secure:false,
     auth:{
-        user:'gurleen0790.cse19@chitkara.edu.in',
+        user:'g@gmail.com',
         pass:'*******'
     }
 });
